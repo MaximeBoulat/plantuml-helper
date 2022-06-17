1e2ad62f1692c53c4410468f7b16cedcdb2b74c8
Maxime Boulat <m@m.com>
2022-06-16 21:24:38 -0700

    Adding package

---
+++ spec/plantuml-helper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PlantumlHelperView from '../lib/plantuml-helper-view';
+
+describe('PlantumlHelperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
